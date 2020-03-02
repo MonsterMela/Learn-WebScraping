6d8a112d2c96c686edd07744fbec4e22fce86d56
MonsterMela <amankumargoswami1@gmail.com>
2020-03-02 14:38:32 +0530

    Second Commit

--- first_try.py
+++ first_try.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import pandas as
+import pandas as pd
+import re
 
 
 url = "https://www.datacamp.com/community/tutorials/web-scraping-using-python"
@@ -20,13 +21,19 @@ all_links = soup.find_all("a")
 #for link in all_links:
  #   print(link.get("href"))
 rows = soup.find_all("tr")
-
+list_rows = []
 for row in rows:
-    row_td = row.find_all('td')
-   #print(row_td)
-    str_cells = str(row_td)
-    cleantext = BeautifulSoup(str_cells, "lxml").get_text()
-    #print(cleantext)
-    df = pd.DataFrame(cleantext)
-    df.head(10)
+    cells = row.find_all('td')
+    str_cells = str(cells)
+    clean = re.compile('<.*?>')
+    clean2 = (re.sub(clean, '',str_cells))
+    list_rows.append(clean2)
+#print(list_rows)
+    #type(clean2)
+df = pd.DataFrame(list_rows)
+#print(df.head(10))
+df1 = df[0].str.split(',', expand=True)
+df1[0] = df1[0].str.strip('[')
+print(df1.head(10))
+#print(df1.head(10))
 #type(row_td)
